a2f762a3740a04534dc6ec1d666993230cd7106f
adeogunesther95-lab <adeogunesther95@gmail.com>
2026-03-17 12:00:33 +0100

    import axios

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import React from "react";
+
+export default function Weather(props) {
+  function showWeather(response) {
+    alert(response.data.main.temp);
+  }
+  let apiKey = "7797aa5c054d2bf0b327b9a77385efb6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
