41fa0fa0400210159f75e8b67721c1bd19f48ff4
adeogunesther95-lab <adeogunesther95@gmail.com>
2026-03-17 12:03:33 +0100

    updated the alert

--- src/Weather.js
+++ src/Weather.js
@@ -3,9 +3,7 @@ import React from "react";
 
 export default function Weather(props) {
   function showWeather(response) {
-    alert(response.data.main.temp);
-  }
-  let apiKey = "7797aa5c054d2bf0b327b9a77385efb6";
+    alert(`It is ${response.data.main.temp}C` in {props.city});
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
   axios.get(apiUrl).then(showWeather);
