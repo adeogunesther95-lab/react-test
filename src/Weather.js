d3eb93d43ab8d53ebfacfb4470c6a9eb0eefae34
adeogunesther95-lab <adeogunesther95@gmail.com>
2026-03-19 04:35:17 +0100

    modify react-loader-spinner color

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import { ThreeDots } from "react-loader-spinner";
 
 export default function Weather(props) {
   function showWeather(response) {
-    alert(`It is ${response.data.main.temp}C in ${props.city}`);
+    alert(`It is ${Math.round(response.data.main.temp)}°C in ${props.city}`);
   }
   let apiKey = "7797aa5c054d2bf0b327b9a77385efb6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
@@ -15,7 +15,7 @@ export default function Weather(props) {
       height={80}
       width={100}
       radius={9}
-      color="red"
+      color="blue"
       ariaLabel="three-dots-loading"
       wrapperStyle={{ margin: "20px" }}
       wrapperClass="custom-loader"
